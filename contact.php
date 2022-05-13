e98fa085c836010b5534f2f66f2cb47cb066d4e3
ThiagoMariuzza <thiagonicomb.xd@gmail.com>
2022-05-13 18:18:01 -0300

    Php to email ✌

--- contact.php
+++ contact.php
@@ -1,66 +1,20 @@
 <?php
 
-// database connection code
-//if(isset($_POST['first_name']))
-//{
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
-'''
-$con = mysqli_connect('127.0.0.1:3306', 'u742194444_admin', 'deftFlaminkDatabase2021','u742194444_deftFlamink');
-
-// check connection
-if(!$con){
-    echo 'Connection error: '. mysqli_connect_error();
-}
-'''
-
-if(!isset($_POST['submit'])
-{
-  echo "error";
-}
-
 // get the post records
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 
-// validate
-if(empty($name)||empty($phone)){
-  echo "no name";
-  exit;
-}
-
 //send email
-// $email_to = 'eze@deftflamink.com'.','.'thiago@deftflamink'.','.'fiore@deftflamink'.','.'danna@deftflamink';
-$email_to = 'thiagonicomb.xd@gmail.com';
-$email_subject = "Nueva Consulta DEFT FLAMINK";
-$email_body = '<table><tr><th>Nombre: </th><th>'.$name.' '.$phone.';
+$email_to = 'deftflamink@gmail.com'.'saludconquiropraxia@gmail.com';
+$email_subject = "Nueva Consulta WEB";
+$email_body = '<table><tr><th>Nombre: </th><th>'.$name.'</th></tr><tr><th>Telefono: </th><th>'.$phone.'</th></tr></table>';
 $headers  = 'MIME-Version: 1.0' . "\r\n";
 $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 $headers .= 'From: Contacto-Pagina@deftflamink.com' . "\r\n" . 'Reply-To: info@deftflamink.com.ar' . "\r\n";
 
 mail($email_to,$email_subject,$email_body,$headers);
-header( 'Location: https://deftflamink.com/index.html#main');
+header( 'Location: https://saludconquiropraxia.com/produccion/?#contact');
 
 //header( 'Location: https://deftflamink.com/');
-//
-// // database insert SQL code
-// $sql = "INSERT INTO contacts (first_name,last_name,phone,email,description) VALUES ('$first_name', '$last_name', '$phone', '$email', '$description')";
-// echo $sql;
-// // insert in database
-//
-// $rs = mysqli_query($con, $sql);
-//
-//
-// if($rs)
-// {
-// 	echo "Contact Records Inserted";
-// }
-// //}
-// else
-// {
-// 	echo "Are you a genuine visitor?" . mysqli_error($con);
-// }
-// mysqli_close($con);
-// echo"lol2";
-//
 
 ?>
